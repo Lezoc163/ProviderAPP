bd6382556e8d267dcebab119b3f704369d452db1
Lezoc163 <3037907347@qq.com>
2021-11-06 11:53:17 +0800

    Apps about Android content providers.

---
+++ app/src/main/java/com/lc/providertest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.lc.providertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private String newId;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addData = findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //添加数据
+                Uri uri = Uri.parse("content://com.lc.databasetest.provider/book");
+                ContentValues values = new ContentValues();
+                values.put("name", "国王的冲突");
+                values.put("author", "乔治.马丁");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
+        Button queryData = findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //查询数据
+                Uri uri = Uri.parse("content://com.lc.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "书名是：" + name);
+                        Log.d(TAG, "作者是：" + author);
+                        Log.d(TAG, "页数是：" + pages);
+                        Log.d(TAG, "价格是：" + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+        Button updateData = findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //更新数据
+                Uri uri = Uri.parse("content://com.lc.databasetest.provider/book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "刀剑风暴");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
+        Button deleteData = findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //删除数据
+                Uri uri = Uri.parse("content://com.lc.databasetest.provider/book/" + newId);
+                getContentResolver().delete(uri, null, null);
+            }
+        });
+    }
+}
